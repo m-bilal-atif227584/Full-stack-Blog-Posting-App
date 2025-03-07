706abdc1bc23701f4ae4eb9d729456d43e4bed6e
m bilal atif227584 <m.bilal227584@gmail.com>
2025-03-07 17:36:13 +0500

    blogapp

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../assets/logo.jpeg'
 
 function Logo() {
   return (
     <div className='rounded-2xl w-24 md:w-36'>
       <p className='text-[#6a6a6a] absolute md:mt-[73px] font-extrabold text-[14px] md:text-xl md:ml-4 ml-2 mt-[50px]'>Post Cloud</p>
-    <img src="src/assets/logo.jpeg" alt="logo" className='rounded-2xl border-[3px] border-white' />
+    <img src={logo} alt="logo" className='rounded-2xl border-[3px] border-white' />
     </div>
   )
 }
